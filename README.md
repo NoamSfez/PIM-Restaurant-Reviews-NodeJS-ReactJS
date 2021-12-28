d3f041946d54ef296c8521178524734b4c1b50bc
NoamSfez <noam.sfez@gmail.com>
2021-12-28 10:07:26 +0200

    comments

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # API-Restaurant-Reviews
 
 \***\*\*\*\*\*\*\***\*\*\*\*\***\*\*\*\*\*\*\***_LINKS_\***\*\*\*\*\*\*\***\*\*\*\*\***\*\*\*\*\*\*\***
+https://www.youtube.com/watch?v=mrHNSanmqQ4
 
 https://cloud.mongodb.com/v2/61b0895c892afd224d38c2cd#metrics/replicaSet/61b08a7d9c6328293e0aa076/explorer/sample_restaurants/reviews/find
 
--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,3 @@ MongoClient.connect(process.env.RESTREVIEWS_DB_URI)
       console.log(`Listening on port ${port}`);
     });
   });
-
-// ../node-course/restaurant-reviews/backend nodemon index.js
-//https://www.youtube.com/watch?v=mrHNSanmqQ4 a 1:05:38
-// https://cloud.mongodb.com/v2/61b0895c892afd224d38c2cd#metrics/replicaSet/61b08a7d9c6328293e0aa076/explorer/sample_restaurants/reviews/find
